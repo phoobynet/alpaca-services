4f063b6e1f346aec4f8360f74fb1870ff3be4ef9
Glenn <phoobynet@users.noreply.github.com>
2022-09-01 20:13:25 +0100

    removed throttle from bars

--- src/marketData/bars/features/streamBars.ts
+++ src/marketData/bars/features/streamBars.ts
@@ -1,6 +1,5 @@
 import { CancelFn } from '@/types'
 import { Bar } from '@/marketData'
-import throttle from 'lodash/throttle'
 import { MarketDataStream } from '@/marketData/stream'
 import { cleanBar } from '@/marketData/bars/helpers'
 
@@ -9,19 +8,13 @@ import { cleanBar } from '@/marketData/bars/helpers'
  * @category Bars
  * @param symbol
  * @param handler
- * @param throttleMs
  */
 export const streamBars = async (
   symbol: string,
   handler: (t: Bar) => void,
-  throttleMs = 500,
 ): Promise<CancelFn> => {
   const cleanHandler = (data: unknown) => {
     handler(cleanBar(data as Bar))
-  }
-  if (throttleMs > 0) {
-    const throttledHandler = throttle(cleanHandler, throttleMs)
-    return await MarketDataStream.subscribeTo(symbol, 'bars', throttledHandler)
   }
   return await MarketDataStream.subscribeTo(symbol, 'bars', cleanHandler)
 }
