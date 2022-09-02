344332c52d4eef95bbe47c3c3a4eb07d2ccec938
Glenn <phoobynet@users.noreply.github.com>
2022-09-02 13:28:05 +0100

    added mutex around alpaca HTTP calls

--- src/marketData/http/cryptoSource.ts
+++ src/marketData/http/cryptoSource.ts
@@ -1,5 +1,6 @@
 import { MarketDataClass, MarketDataSource } from '@/marketData/types'
 import { createHttpClient, HttpClient } from '@/http'
+import { Mutex } from 'async-mutex'
 
 let httpClient: HttpClient
 
@@ -16,18 +17,28 @@ const getHttpClient = (): HttpClient => {
   return httpClient
 }
 
+// due to limitations of the Alpaca Crypto API, we need to use a mutex
+// to prevent 429
+const httpGetMutex = new Mutex()
+
 /**
  * @group Market Data
  * @category HTTP
  */
 export const cryptoSource: MarketDataSource = {
   async get<T>(url: string, queryParams?: Record<string, string>): Promise<T> {
-    const httpResponse = await getHttpClient().get<T>(url, queryParams)
+    const release = await httpGetMutex.acquire()
 
-    if (httpResponse.ok) {
-      return httpResponse.data as T
-    } else {
-      throw new Error(httpResponse.message)
+    try {
+      const httpResponse = await getHttpClient().get<T>(url, queryParams)
+
+      if (httpResponse.ok) {
+        return httpResponse.data as T
+      } else {
+        throw new Error(httpResponse.message)
+      }
+    } finally {
+      release()
     }
   },
   type: MarketDataClass.crypto,
